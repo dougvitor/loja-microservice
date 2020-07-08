5d216bdc47a042f74667c1951c54d78a1a1f812d
Douglas Vitor França da Silva <douglas.vitor@totvs.com.br>
2020-07-07 23:23:10 -0300

    Aula 02 -  Service Registry com Eureka
    O que foi feito:
    
       - A integração entre microsserviços com RestTemplate
            - O RestTemplate do Spring permite chamadas HTTP de alto nível
    
        - Um introdução ao Service discovery e Service registry
            - Service registry é um servidor central, onde todos os microsserviços ficam cadastrados (nome e IP/porta)
            - Service discovery é um mecanismo de descoberta do IP do microsserviço pelo nome
            - Dessa forma, nenhum microsserviço fica acoplado ao outro pelo IP/porta
        - A implementação do service registry através do Eureka Server
        - Como registrar da Loja e do Fornecedor no Eureka Server
        - A resolução do IP/porta através do nome do microsserviço nas requisições

--- src/main/java/br/com/home/microservice/loja/controller/CompraController.java
+++ src/main/java/br/com/home/microservice/loja/controller/CompraController.java
@@ -1,19 +1,24 @@
 package br.com.home.microservice.loja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.home.microservice.loja.dto.CompraDTO;
+import br.com.home.microservice.loja.service.CompraService;
 
 @RestController
 @RequestMapping("/compra")
 public class CompraController {
 	
+	@Autowired
+	private CompraService service;
+	
 	@PostMapping
 	public void realizarCompra(@RequestBody CompraDTO compra) {
-		
+		service.realizarCompra(compra);
 	}
 
 }
---
+++ src/main/java/br/com/home/microservice/loja/dto/InfoFornecedorDTO.java
@@ -0,0 +1,14 @@
+package br.com.home.microservice.loja.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InfoFornecedorDTO {
+	
+	private String endereco;
+
+}
